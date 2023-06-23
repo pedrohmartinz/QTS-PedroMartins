4e66ff374bfbfc3009f45239de0fe4c07d04c534
pedrohmartinz <pedrohmartinso@gmail.com>
2023-06-23 07:35:52 -0300

    Atividade dos assert.

--- Tarefas/exercicio-assert01.js
+++ Tarefas/exercicio-assert01.js
@@ -2,21 +2,27 @@ import {
     assertExists, 
     assertNotEquals, 
     assertThrows, 
-    assertThrowsAsync,
     assertMatch } 
 from 'https://deno.land/std/testing/asserts.ts';
 
-// Exemplo 1: assertExists
-const objeto = { nome: 'João', idade: 25 };
-assertExists(objeto.nome);
 
+
+// Exemplo 1: assertExists
+Deno.test("Teste de existencia (API)", () => {
+  const objeto = { nome: 'João', idade: 25 };
+  assertExists(objeto.nome);
+})
 
 // Exemplo 2: assertNotEquals
-const resultado = 5 * 5;
-assertNotEquals(resultado, 20);
+Deno.test("Teste de resultados não esperados (API)", () => {
+  const resultado = 5 * 5;
+  assertNotEquals(resultado, 20);
+})
+
 
 // Exemplo 3: assertThrows
-function dividir(a, b) {
+Deno.test("Testando divisão por 0", () => {
+  function dividir(a, b) {
     if (b === 0) {
       throw new Error('Divisão por zero');
     }
@@ -26,20 +32,17 @@ function dividir(a, b) {
   assertThrows(() => {
     dividir(10, 0);
   });
+})
+
+
 
 // Exemplo 4: assertMatch
-const texto = 'Olá, mundo!';
-assertMatch(texto, /mundo/);
+Deno.test("Testando se os valores são iguais", () => {
+    const texto = 'Olá, mundo!';
+    assertMatch(texto, /mundo/);
+})
 
-// Exemplo 5: assertThrowsAsync
-async function obterDados() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error('Erro ao obter dados'));
-    }, 2000);
-  });
-}
 
-await assertThrowsAsync(async () => {
-  await obterDados();
-});
+
+
+
